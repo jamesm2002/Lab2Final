d87b1d3f2e5d6e69c011fd1b84155f387f1d69d9
jamesm2002 <91893668+jamesm2002@users.noreply.github.com>
2021-11-24 21:41:36 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+//creating app
+const app = express();
+//handling static HTML and EJS templates
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("index"); //no need for ejs extension
+});
+//route for contacts
+app.get("/contacts", (req, res) => {
+  res.render("contacts");
+});
+//pass requests to the router middleware
+const router = require("./routes/post");
+app.use(router);
+
+//make the app listen on port
+const port = process.argv[2] || process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`Cart app listening at http://localhost:${port}`);
+});
---
+++ controllers/productController.js
@@ -0,0 +1,31 @@
+const getCatalogue = (request, response) => {
+  const catalogueServices = require("../services/productServices");
+  catalogueServices.searchService(function (err, rows) {
+    response.json(rows);
+    response.end();
+  });
+};
+
+const getProductByID = (request, response) => {
+  const catalogueServices = require("../services/productServices");
+  let reference = request.params.reference;
+  catalogueServices.searchIDService(reference, function (err, rows) {
+    response.json(rows);
+    response.end();
+  });
+};
+
+const getProductsByCategory = (request, response) => {
+  const catalogueServices = require("../services/productServices");
+  let reference = request.params.category;
+  catalogueServices.searchCategoryService(category, function (err, rows) {
+    response.json(rows);
+    response.end();
+  });
+};
+
+module.exports = {
+  getCatalogue,
+  getProductByID,
+  getProductsByCategory
+};
